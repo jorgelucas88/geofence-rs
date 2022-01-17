0af626ac9527ccb4476b0c3cec326bad5814f822
jorgelucas88 <jlb.lucas88@gmail.com>
2022-01-17 16:54:38 +0100

    API Rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 import { GeofenceModule } from './geofence/geofence.module';
 
 @Module({
-  imports: [GeofenceModule],
+  imports: [GeofenceModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    })],
   controllers: [],
-  providers: [],
+  providers: [ {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+  }],
 })
 export class AppModule {}
